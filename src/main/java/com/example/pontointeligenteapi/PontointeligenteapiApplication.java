72339138bf52c4127b4359f657daeb01bd9f8b71
Alberto Costa <mihcadevs@gmail.com>
2020-08-26 23:16:30 -0300

    Criação do projeto

---
+++ src/main/java/com/example/pontointeligenteapi/PontointeligenteapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pontointeligenteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontointeligenteapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontointeligenteapiApplication.class, args);
+	}
+
+}
