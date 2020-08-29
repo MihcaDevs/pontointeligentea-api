9da6390cb55bb6a655f4ef70985c98aad93fe38a
Alberto Costa <mihcadevs@gmail.com>
2020-08-28 21:15:01 -0300

    Adicionando novamente a classe principal de teste com o profile configurado

---
+++ src/test/java/com/example/pontointeligenteapi/RestSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.pontointeligenteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class RestSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
