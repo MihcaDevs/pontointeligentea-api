b90790f5da6aba4a65e2d8193906e2820ab0c1b4
Alberto Costa <mihcadevs@gmail.com>
2020-08-30 00:19:50 -0300

    implementação do springsecurity, testes unitarios ok, e teste no postman ok

---
+++ src/main/java/com/example/pontointeligenteapi/security/services/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.pontointeligenteapi.security.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.pontointeligenteapi.entities.Funcionario;
+import com.example.pontointeligenteapi.security.JwtUserFactory;
+import com.example.pontointeligenteapi.services.FuncionarioService;
+
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<Funcionario> funcionario = funcionarioService.buscarPorEmail(username);
+
+		if (funcionario.isPresent()) {
+			return JwtUserFactory.create(funcionario.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
--- src/test/java/com/example/pontointeligenteapi/controllers/LancamentoControllerTest.java
+++ src/test/java/com/example/pontointeligenteapi/controllers/LancamentoControllerTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -28,6 +29,8 @@ import com.example.pontointeligenteapi.services.FuncionarioService;
 import com.example.pontointeligenteapi.services.LancamentoService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -94,6 +97,15 @@ public class LancamentoControllerTest {
 				.andExpect(status().isOk());
 	}
 	
+	@Test
+	@WithMockUser
+	public void testRemoverLancamentoAcessoNegado() throws Exception {
+		BDDMockito.given(this.lancamentoService.buscarPorId(Mockito.anyLong())).willReturn(Optional.of(new Lancamento()));
+
+		mvc.perform(MockMvcRequestBuilders.delete(URL_BASE + ID_LANCAMENTO)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isForbidden());
+	}
 
 	private String obterJsonRequisicaoPost() throws JsonProcessingException {
 		LancamentoDto lancamentoDto = new LancamentoDto();
